ba137d4c69eb1c0048f8d4ecd0b23245485cdc0c
Blake Bless and DJ Roychoudhury <blake@>
2016-04-06 11:58:45 -0700

    outputs random numbers from array

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,16 @@
 // player one
 function PlayerOne(num){
 this.num = num;
-var numArray = [];
-numArray.push(num)
 }
 
 
 
 
 // playertwo
-function PlayerTwo(numArray2){
-this.numArray2 = [];
+function PlayerTwo(num2){
+  this.num2 = num2;
+  var numArray = [];
+  numArray.push(num2)
 }
 
 
@@ -27,19 +27,42 @@ this.numArray2 = [];
 // User
 $(document).ready(function(event){
 
+    var numArray = [];
+
   $(".playerinputOne").submit(function(event){
     event.preventDefault();
-    var num = Math.floor((Math.random() * 6) + 1);
-    var newPlayer = new PlayerOne(num);
 
-  // var randomNum =
-  console.log(num);
-  console.log(newPlayer);
+    var num = Math.floor((Math.random() * 6) + 1);
+
+    numArray.push(num);
+
+    console.log(numArray);
+
+    if (num > 1) {
+      $("#turntotal1").text(numArray);
+    } else if (num === 1){
+      alert("You got a One. No points added, Player 2 roll.");
+      $(".playerinputOne").hide();
+    }
 });
 
-  $("form.playerinputTwo").submit("button#roll2");
-  var randomNum2 = Math.floor((Math.random() * 6) + 1);
 
+
+
+
+
+
+
+
+  $(".btn .btn-danger").submit(function(event){;
+  event.preventDefault();
+
+  var num2 = Math.floor((Math.random() * 6) + 1);
+  var newPlayer2 = new PlayerTwo(num2);
+
+  console.log(num2);
+  console.log(newPlayer2);
+});
   $("form.playerinputOne").submit("button#hold1");
 
 
