ed2e135d0ff0c9fc50201a38ad311562982dff1f
Blake Bless and DJ Roychoudhury <blake@>
2016-04-06 15:21:33 -0700

    player two now works takes playes sum and adds it to grand total.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 // Business
 
-
-
 // player one
 function PlayerOne(num){
 this.num = num;
 }
-
-
 
 
 // playertwo
@@ -21,52 +17,68 @@ function PlayerTwo(num2){
 
 
 
-
-
-
 // User
-$(document).ready(function(event){
-
-    var numArray = [];
-
-  $(".playerinputOne").submit(function(event){
-    event.preventDefault();
-
-    var num = Math.floor((Math.random() * 6) + 1);
-
-    numArray.push(num);
-
-    console.log(numArray);
-
-    if (num > 1) {
-      $("#turntotal1").text(numArray);
-    } else if (num === 1){
-      alert("You got a One. No points added, Player 2 roll.");
-      $(".playerinputOne").hide();
-    }
-});
 
 
+var numArray = [];
 
-
-
-
-
-
-
-  $(".btn .btn-danger").submit(function(event){;
+$(".buttonroll1").submit(function(event){
   event.preventDefault();
+  var num = Math.floor((Math.random() * 6) + 1);
+  numArray.push(num);
 
-  var num2 = Math.floor((Math.random() * 6) + 1);
-  var newPlayer2 = new PlayerTwo(num2);
-
-  console.log(num2);
-  console.log(newPlayer2);
+  if (num > 1) {
+    $("#turntotal1").text(numArray);
+  } else if (num === 1){
+    alert("You got a One. No points added, Player 2 roll.");
+    numArray.splice(0, numArray.length)
+    // $(".buttonroll1").hide();
+    $("#turntotal1").text(numArray);
+  }
 });
-  $("form.playerinputOne").submit("button#hold1");
+
+$(".buttonhold1").submit(function(event){
+  event.preventDefault();
+  var sum = 0;
+  var myResult = numArray;
+  myResult.forEach(function(num){
+    sum += num;
+  });
+  $("#outputtotal1").text(sum)
+  numArray.splice(0, numArray.length)
+  // $(".buttonroll1").hide();
+  $("#turntotal1").text(numArray);
+});
 
 
-  $("form.playerinputTwo").submit("button#hold2");
+// player 2
 
+
+$(".buttonroll2").submit(function(event){
+  event.preventDefault();
+  var num = Math.floor((Math.random() * 6) + 1);
+  numArray.push(num);
+
+  if (num > 1) {
+    $("#turntotal2").text(numArray);
+  } else if (num === 1){
+    alert("You got a One. No points added, Player 1 roll.");
+    numArray.splice(0, numArray.length)
+    // $(".buttonroll1").hide();
+    $("#turntotal2").text(numArray);
+  }
+});
+
+$(".buttonhold2").submit(function(event){
+  event.preventDefault();
+  var sum = 0;
+  var myResult = numArray;
+  myResult.forEach(function(num){
+    sum += num;
+  });
+  $("#outputtotal2").text(sum)
+  numArray.splice(0, numArray.length)
+  // $(".buttonroll1").hide();
+  $("#turntotal2").text(numArray);
 
 });
